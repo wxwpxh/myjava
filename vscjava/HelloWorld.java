38cd831213a634b4de69c5639c755039c8821c35
wxwpxh <15305732366@189.cn>
2021-08-01 21:22:29 +0800

    update

---
+++ vscjava/HelloWorld.java
@@ -0,0 +1,15 @@
+//javac HelloWorld.java -encoding gb2312
+public class HelloWorld {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		for (int i = 0; i < 5; i++) {
+			System.out.println("I love you!");
+			
+		}
+
+	}
+
+}
