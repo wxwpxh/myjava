dc7a420350a0e47f5bb32f5d90038a86c3ca89ca
wxwpxh <15305732366@189.cn>
2021-08-01 21:43:54 +0800

    update

--- vscjava/HelloWorld1.java
+++ vscjava/HelloWorld1.java
@@ -5,14 +5,13 @@ public class HelloWorld1 {
 	 * @param args
 	 */
 	public static void main(String[] args) {
-		int sum=0;
-		for (int i = 0; i < 10; i++) {
-			sum=sum+i;			
+		int sum = 0;
+		for (int i = 0; i <= 10; i++) {
+			sum = sum + i;
 		}
-		
+
 		System.out.print("sum=");
 		System.out.println(sum);
-
 
 	}
 
